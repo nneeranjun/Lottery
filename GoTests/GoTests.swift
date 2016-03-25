f45a029d1c9aa2b3721515f7ec7cbbf934ca5e02
Nilay Neeranjun <Nilay@nilays-mac-mini.local>
2016-03-24 20:26:45 -0400

    Initial Commit

---
+++ GoTests/GoTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoTests.swift
+//  GoTests
+//
+//  Created by Nilay Neeranjun on 3/24/16.
+//  Copyright © 2016 Nilay Neeranjun. All rights reserved.
+//
+
+import XCTest
+@testable import Go
+
+class GoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
